311d1d51946fcd8fc9e138ca517d4b49c7647fcc
Sam11360 <baleclyps@hotmail.fr>
2017-06-12 17:58:48 +0200

    page contact

---
+++ accueil.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Challenge 2017</title>
+  </head>
+  <body>
+    <header class="header">
+            <?php
+               include("includes/header.php");
+              ?>
+       </header>
+
+
+       <div class="footer">
+        <?php
+           include("includes/footer.php");
+          ?>
+       </div>
+
+
+
+
+  </body>
+</html>
--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
-<div class='conteneur'>
+<div class="conteneur">
 <div class="contact">
     <p><a href="contact.php">Contact</a></p>
   </div>
