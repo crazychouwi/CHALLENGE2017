fc7a76eb1311381c4e83313b79b7d65bf681acb0
Sam11360 <baleclyps@hotmail.fr>
2017-06-12 18:51:57 +0200

    modif css couleur contact

--- contact.php
+++ contact.php
@@ -6,7 +6,8 @@
     <title>Contact</title>
   </head>
   <body>
-    <div class="contact">
+    <h1>Contact</h1>
+    <div class="contacts">
      <ul>
        <li>Nicolas Villa</li>
        <li>Formateur/Facilitateur École Régionale du Numérique INESS</li>
@@ -29,6 +30,12 @@
 
     <div class="map">
    <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m11!1m3!1d20384.295043716134!2d2.969329854056524!3d43.18003977879723!2m2!1f0!2f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b1ac52defc6925%3A0xb10f76c5fd1d874a!2sIN&#39;ESS+Le+Grand+Narbonne!5e1!3m2!1sfr!2sfr!4v1488979146577" width="600" height="450" style="border:5" allowfullscreen></iframe>
+   </div>
+
+   <div class="footer">
+    <?php
+       include("includes/footer.php");
+      ?>
    </div>
 
 
