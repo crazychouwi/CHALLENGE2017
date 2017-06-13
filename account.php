b675b9713fd3c30b8ba51d885f91d2e689496a99
Emilie Fernandez <emilie@localhost.localdomain>
2017-06-13 15:52:16 +0200

    ajout du php connex a la bdd

--- account.php
+++ account.php
@@ -1,4 +1,4 @@
-<DOCTYPE! HTML>
+<!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
@@ -7,36 +7,38 @@
  <link href="assets/css/connection.css" type="text/css" rel="stylesheet">
   </head>
   <body>
-<form method="post" action=".php" enctype="multipart/form-data">
+
 
   <div class="header">
     <img src="assets/img/logoSimplon.png" alt="logoSimplon">
   </div>
 
         <div class="connexion">
-     <h1>créer ton compte</h1>
-<p>déja inscrit  <a href="login.php">se connecter</a></p>
-    <label for="nom">Nom :</label><br/>
-   <input type="text" name="username" id="nom" /><br/>
+         <h1>créer ton compte</h1>
+          <p>déja inscrit  <a href="login.php">se connecter</a></p>
+    <form method="post" action="traitement.php" enctype="multipart/form-data">
 
-   <!-- <label for="prenom">Prénom:</label><br/>
-   <input type="prenom" name="prenom" id="prenom" /><br/> -->
+    <label for="nom">Nom :</label><br/>
+    <input type="text" required="required" name="username" id="nom" /><br/>
+
+   <label for="prenom">Prénom :</label><br/>
+   <input type="text" name="prenom" id="prenom" /><br/>
+
+   <label class="label" for="login">Login :</label>
+   <input type="text" name="login" id="login" />
 
     <label for="email">Email :</label><br/>
-    <input type="email" name="email" id="email" /><br/>
+    <input type="email" required="required" name="email" id="email" /><br/>
 
-    <label for="">password :</label><br/>
-    <input type="password" name="password" id="pw"><br/>
+    <label for="password">Password :</label><br/>
+    <input type="password" required="required" name="password" id="pw"><br/>
 
-    <label for=""> vérification password :</label><br/>
-    <input type="password" name="vpw" id="vpassword"><br/>
+    <label for="confirmation">Vérification password :</label><br/>
+    <input type="password" required="required" name="password" id="vpassword"><br/>
 
-<div id="forgot"><img src="assets/img/pt.jpg" id="point"><a href="forgot">mot de passe oublié</a></div>
+    <div id="forgot"><img src="assets/img/pt.jpg" id="point"><a href="forgot">mot de passe oublié</a></div>
 
-  <input id="button" type="submit" value="Sign In">
-
-
-
+    <input id="button" type="submit" name="commit" value="Sign In">
 
   <div class="footer">
    <?php
