cad398fde5c8962d8713e81a67ec265fd41b073a
elo <eloisemecozzi@hotmail.fr>
2017-06-13 09:43:04 +0200

    ajout page crédits

---
+++ credit.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="./assets/css/contact.css"/>
+    <title>Crédits</title>
+  </head>
+  <body>
+    <div class="header">
+      <img src="assets/img/logoSimplon.png" alt="logoSimplon">
+    </div>
+    <h1 class="headline">Crédits</h1>
+    <h2>Ecole Régionale du Numérique SIMPLON</h2>
+    <div class="contacts">
+     <ul>
+       <li>Jennifer AZINHEIRA : jny66000@gmail.com</li>
+       <li>Laurelou BOUCHAREU : laureloobagatelle@hotmail.fr</li>
+       <li>Romain COSSON: romain.cosson11@live.fr</li>
+       <li>Axel DELBOURG : ad.joblink@gmail.com</li>
+       <li>Emilie FERNANDEZ : fernandezemilie@ymail.com</li>
+       <li>Sandra GUILLEMIN : ptitbou.d_avril@hotmail.fr</li>
+       <li>Frédéric GUIRAUD : kiluaws@gmail.com</li>
+       <li>Yvan JANSKY : jansky@yahoo.com</li>
+       <li>Bruno LAPIERRE : lapierre.bruno@gmail.com</li>
+       <li>Marlène Mas : baleclyps@gmail.com</li>
+       <li>Eloise MECOZZI : eloisemecozzi@gmail.com</li>
+       <li>Ange MONDOLONI : swinginlover@hotmail.com</li>
+       <li>Hermeline ROUSSELET : biboulle66@gmail.com</li>
+       <li>Anne SERRANO : anne.serrano@hotmail.fr</li>
+   </div>
+
+   <div class="footer">
+    <?php
+       include("includes/footer.php");
+      ?>
+   </div>
+  </body>
+</html>
