4372609facfd548ba7f731727804bd067568fce9
kilua <kilua@localhost.localdomain>
2017-06-13 11:51:58 +0200

    ajout formq.php

---
+++ formq.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>questionnaire</title>
+		<style type="text/css" src="/assets/css/style.css"></style>
+	</head>
+	<body>
+		<h1>Dossier individuel du stagiaire</h1>
+		<p>
+			Répondez aux questions que vous souhaitez pré-remplir, puis cliquez sur "Envoyer".
+		</p>
+		<form action="traitement_formulaire.php" method="post" name="questionnaire">
+			<h2>Partie à remplir par l'organisme de formation Simplon.co</h2>
+			<p>
+				Information collective du:
+			</p>
+			<label for="">Votre Réponse</label>
+			<input type="text" name="" id=""/>
+			<p>
+				Reçu par:
+			</p>
+			<label for="">Votre Réponse</label>
+			<input type="text" name="" id=""/>
+			<p>
+				Reçu le:
+			</p>
+			<label for="">Mois,jour,année</label>
+			<input type="date" name="" id=""/>
+			<p>
+				Date de début de parcours:
+			</p>
+			<label for="">Mois,jour,année</label>
+			<input type="date" name="" id=""/>
+			<p>
+				Date de fin de parcours:
+			</p>
+			<label for="">Mois,jour,année</label>
+			<input type="date" name="" id=""/>
+		</form>
+		<form action="traitement_candidat.php" method="post" name="questionnaire">
+			<h2>Partie à remplir par le candidat</h2>
+			<p>
+				Description (facultative)
+			</p>
+			<label for=""></label>
+			<p>
+				Question:
+			</p>
+			<input type="checkbox" name="choix1" value="1">
+			Monsieur
+			<input type="checkbox" name="choix2" value="2">
+			Madame
+			<input type="checkbox" name="choix3" value="3">
+			Mademoiselle
+			<p>
+				Nom:
+			</p>
+			<label for="">Votre Réponse</label>
+			<input type="text" name="" id=""/>
+			<p>
+				Prénom:
+			</p>
+			<label for="">Votre Réponse</label>
+			<input type="text" name="" id=""/>
+			<p>
+				Nom de jeune fille:
+			</p>
+			<label for="">Votre Réponse</label>
+			<input type="text" name="" id=""/>
+			<p>
+				Date de naissance:
+			</p>
+			<label for="">Mois,jour,année</label>
+			<input type="date" name="" id=""/>
+			<p>
+				Age
+			</p>
+			<label for="">Votre Réponse</label>
+			<input type="text" name="" id=""/>
+			<p>
+				Lieu de naissance
+			</p>
+			<label for="">Votre Réponse</label>
+			<input type="text" name="" id=""/>
+			<p>
+				Pays de naissance:
+			</p>
+			<label for="">Votre Réponse</label>
+			<input type="text" name="" id=""/>
+			<p>
+				Nationalité
+			</p>
+			<label for="">Votre Réponse</label>
+			<input type="text" name="" id=""/>
+			<p>
+				Date d'entrée en France:
+			</p>
+			<label for="">Mois,jour,année</label>
+			<input type="date" name="" id=""/>
+			<p>
+				Question:
+			</p>
+			<input type="checkbox" name="choix1" value="1">
+			Marié(e)
+			<input type="checkbox" name="choix2" value="2">
+			Célibataire
+			<input type="checkbox" name="choix3" value="3">
+			Divorcé(e)
+			<input type="checkbox" name="choix4" value="4">
+			Autre
+			<p>
+				Nombre d'enfants:
+			</p>
+			<label for="">Votre Réponse</label>
+			<input type="text" name="" id=""/>
+			<p>
+				Age de(s) l'enfant(s):
+			</p>
+			<label for="">Votre Réponse</label>
+			<input type="text" name="" id=""/>
+			<p>
+				Lieu d'hébergement:
+			</p>
+			<input type="checkbox" name="choix1" value="1">
+			Famille
+			<input type="checkbox" name="choix2" value="2">
+			Amis
+			<input type="checkbox" name="choix3" value="3">
+			Foyer
+			<input type="checkbox" name="choix4" value="4">
+			Autre
+			<input type="checkbox" name="choix5" value="5">
+			Logement indépendant
+			<p>
+				N° personnel CPAM:
+			</p>
+			<label for="">Votre Réponse</label>
+			<input type="text" name="" id=""/>
+			<p>
+				N° CMU:
+			</p>
+			<label for="">Votre Réponse</label>
+			<input type="text" name="" id=""/>
+			<p>
+				Date de fin de validité du titre de séjour ou de la carte d'identité:
+			</p>
+			<label for="">Mois,jour,année</label>
+			<input type="date" name="" id=""/>
+			<p>
+				Adresse:
+			</p>
+			<label for="">Votre Réponse</label>
+			<input type="text" name="" id=""/>
+			<p>
+				Code postal:
+			</p>
+			<label for="">Votre Réponse</label>
+			<input type="text" name="" id=""/>
+			<p>
+				Ville:
+			</p>
+			<label for="">Votre Réponse</label>
+			<input type="text" name="" id=""/>
+			<p>
+				Quartier:
+			</p>
+			<label for="">Votre Réponse</label>
+			<input type="text" name="" id=""/>
+			<p>
+				Téléphone fixe:
+			</p>
+			<label for="">Votre Réponse</label>
+			<input type="text" name="" id=""/>
+			<p>
+				Portable:
+			</p>
+			<label for="">Votre Réponse</label>
+			<input type="text" name="" id=""/>
+			<p>
+				Email
+			</p>
+			<label for="">Votre Réponse</label>
+			<input type="email" name="" id=""/>
+			<p>
+				Personne à prévenir en cas d'urgence
+			</p>
+			<label for="">Votre Réponse</label>
+			<input type="text" name="" id=""/>
+			<p>
+				Nom Prénom:
+			</p>
+			<label for="">Votre Réponse</label>
+			<input type="text" name="" id=""/>
+			<p>
+				Tél:
+			</p>
+			<label for="">Votre Réponse</label>
+			<input type="text" name="" id=""/>
+		</body>
+	</html>
+
