1639d48f0a118e632b3da835b508bd212f24b9df
elo <eloisemecozzi@hotmail.fr>
2017-06-13 10:18:27 +0200

    modif page crédits

--- credit.php
+++ credit.php
@@ -10,23 +10,24 @@
       <img src="assets/img/logoSimplon.png" alt="logoSimplon">
     </div>
     <h1 class="headline">Crédits</h1>
-    <h2>Ecole Régionale du Numérique SIMPLON</h2>
     <div class="contacts">
-     <ul>
-       <li>Jennifer AZINHEIRA : jny66000@gmail.com</li>
-       <li>Laurelou BOUCHAREU : laureloobagatelle@hotmail.fr</li>
-       <li>Romain COSSON: romain.cosson11@live.fr</li>
-       <li>Axel DELBOURG : ad.joblink@gmail.com</li>
-       <li>Emilie FERNANDEZ : fernandezemilie@ymail.com</li>
-       <li>Sandra GUILLEMIN : ptitbou.d_avril@hotmail.fr</li>
-       <li>Frédéric GUIRAUD : kiluaws@gmail.com</li>
-       <li>Yvan JANSKY : jansky@yahoo.com</li>
-       <li>Bruno LAPIERRE : lapierre.bruno@gmail.com</li>
-       <li>Marlène Mas : baleclyps@gmail.com</li>
-       <li>Eloise MECOZZI : eloisemecozzi@gmail.com</li>
-       <li>Ange MONDOLONI : swinginlover@hotmail.com</li>
-       <li>Hermeline ROUSSELET : biboulle66@gmail.com</li>
-       <li>Anne SERRANO : anne.serrano@hotmail.fr</li>
+      <h2>Ecole Régionale du Numérique SIMPLON</h2>
+         <ul>
+           <li>Jennifer AZINHEIRA : jny66000@gmail.com</li>
+           <li>Laurelou BOUCHAREU : laureloobagatelle@hotmail.fr</li>
+           <li>Romain COSSON: romain.cosson11@live.fr</li>
+           <li>Axel DELBOURG : ad.joblink@gmail.com</li>
+           <li>Emilie FERNANDEZ : fernandezemilie@ymail.com</li>
+           <li>Sandra GUILLEMIN : ptitbou.d_avril@hotmail.fr</li>
+           <li>Frédéric GUIRAUD : kiluaws@gmail.com</li>
+           <li>Yvan JANSKY : jansky@yahoo.com</li>
+           <li>Bruno LAPIERRE : lapierre.bruno@gmail.com</li>
+           <li>Marlène Mas : baleclyps@gmail.com</li>
+           <li>Eloise MECOZZI : eloisemecozzi@gmail.com</li>
+           <li>Ange MONDOLONI : swinginlover@hotmail.com</li>
+           <li>Hermeline ROUSSELET : biboulle66@gmail.com</li>
+           <li>Anne SERRANO : anne.serrano@hotmail.fr</li>
+         </ul>
    </div>
 
    <div class="footer">
