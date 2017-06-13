1bf1e18fc1a3229a82b03373db095d91fba334c8
Sam11360 <baleclyps@hotmail.fr>
2017-06-13 13:09:59 +0200

    modif boutons menu

--- includes/menu.php
+++ includes/menu.php
@@ -1,9 +1,15 @@
 <nav>
   <ul>
   <li><a href="index.php" title= "accueil">Accueil</a></li>
-  <li><a href="" title= "connexion">?</a>Connexion</li>
-  <li><a href="" title= "formulaire">?</a>Formulaire</li>
-  <li><a href="" title= "questionnaire">?</a>Questionnaire</li>
-  <li><a href="" title= "">?</a></li>
+  <li><a href="" title= "connexion">Connexion</a></li>
+  <li><a href="" title= "formulaire">Formulaire</a></li>
+  <li><a href="" title= "questionnaire">Questionnaire</a></li>
 </ul>
 </nav>
+<div class="boutons">
+  <p><a href="#">Accueil</a> </p>
+  <p><a href="#">Connexion</a></p>
+  <p><a href="#">Formulaire</a></p>
+  <p><a href="#">Questionnaire</a></p>
+</div>
+</div>
