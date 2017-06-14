1eda604143f7b65e9a7d8765a32a2432e6fa829b
Sam11360 <baleclyps@hotmail.fr>
2017-06-14 11:11:34 +0200

    menu

--- includes/menu.php
+++ includes/menu.php
@@ -1,11 +1,3 @@
-<nav>
-  <ul>
-  <li><a href="index.php" title= "accueil">Accueil</a></li>
-  <li><a href="" title= "connexion">Connexion</a></li>
-  <li><a href="" title= "formulaire">Formulaire</a></li>
-  <li><a href="" title= "questionnaire">Questionnaire</a></li>
-</ul>
-</nav>
 <div class="boutons">
   <p><a href="#">Accueil</a> </p>
   <p><a href="#">Connexion</a></p>
---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>challenge2017</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/contact.css"/>
+  </head>
+  <body>
+
+    <div class="menu">
+            <?php
+               include("includes/menu.php");
+              ?>
+       </div>
+       
+    <div class="header">
+      <img src="assets/img/logoSimplon.png" alt="logoSimplon">
+    </div>
+    <div class="contenu">
+      <h1 class="headline">BIENVENUE</h1>
+      <h1>Vous êtes</h1>
+      <div class="bouton">
+        <div class="formateur">
+          <button type="submit">Formateur</button>
+        </div>
+        <div class="candidat">
+          <button type="submit">Candidat</button>
+        </div>
+      </div>
+    </div>
+    <div class="footer">
+     <?php
+        include("includes/footer.php");
+       ?>
+    </div>
+
+  </body>
+</html>
