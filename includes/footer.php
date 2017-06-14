b082ad1ef5a27fd5f7388b42bc9d0828cc89c5a1
Sam11360 <baleclyps@hotmail.fr>
2017-06-14 11:35:28 +0200

    ajout boutons contact crédit

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,4 @@
-<div class="conteneur">
-<div class="contact">
-    <p><a href="contact.php">Contact</a></p>
+<div class="boutons">
+    <p><a href="contact.php">Contact</a> </p>
+    <p><a href="credit.php">Crédit</a></p>
   </div>
-  <div class="credit">
-      <p><a href="credit.php">Crédit</a></p>
-    </div>
-</div>
