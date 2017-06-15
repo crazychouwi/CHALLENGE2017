8c0a0fe3104604395d892bfade469a7aad19af63
nunkabuk <laureloobagatelle@hotmail.fr>
2017-06-15 13:24:40 +0200

    css en cours

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="./assets/css/contact.css"/>
+
     <link rel="stylesheet" href="./assets/css/style.css"/>
     <title>Contact</title>
   </head>
@@ -18,15 +18,12 @@
                include("includes/header.php");
               ?>
        </div>
-    <h1 class="headline">CONTACT</h1>
-    <div class="contacts">
-    <p>Adresse:</br>
-      55 rue de Vincennes 93100 Montreuil</br>
-      Téléphone: 0186956453</br>
-      Email:inbox@simplon.co</p>
+
+    <div class="contact">
+<h1>contact</h1>
+    Téléphone: 0186956453</br>
+      <p>Email:inbox@simplon.co</p>
    </div>
-
-
 
     <div class="map">
    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1959.594222775552!2d2.4352611!3d48.8542176!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66d5f8a4cb333%3A0x9535ab9bdafaa332!2s55+Rue+de+Vincennes%2C+93100+Montreuil!5e1!3m2!1sfr!2sfr!4v1497388343207" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
